2a73e88a120e7dedb3a93e8b0322d26d871ffad5
Michael Burnstein <michael@burnstein.com>
2017-04-19 04:35:09 +0000

    Lesson show page

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,12 @@
+class LessonsController < ApplicationController
+  def show
+  end
+
+
+  Private
+
+  helper_method :current_lesson
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
+end
