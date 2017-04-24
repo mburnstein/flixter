eea7d271c16307ed9cb3847dc7760da590acb141
Michael Burnstein <michael@burnstein.com>
2017-04-24 20:04:48 +0000

    Fixing images problem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"] ,
+    :region => 'us-west-1'                     # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
