e79121b997ffc7cc61d6610c92eb5c0138f0ae2f
Michael Burnstein <michael@burnstein.com>
2017-05-13 23:17:40 +0000

    Require enrolled to view a lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
-
   private
+
+   def require_authorized_for_current_course
+    if current_lesson.section.course.user != current_user
+      redirect_to course_path, alert: 'You must be enrolled in the course to view this lessons' 
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
